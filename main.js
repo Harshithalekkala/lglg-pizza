25591fd386021feb489aa0dc3aecd781dcafcbba
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2021-09-05 14:59:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+menu_list_array = ["Veg Margherita Pizza",
+                    "chicken tandoori pizza",
+                    "veg supreme pizza",
+                    "paneer tikka pizza",
+                    "deluxe veggie pizza",
+                  "veg extravaganza pizza" ];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for (var i = 0; i <menu_list_array.length; i++) {
+    htmldata =htmldata+ '<li>' + menu_list_array[i] + '</li>'
+    
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
